2edbe2c1ae8162907a1f727b4d86962b1a2e1bc8
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2023-02-01 15:17:58 +0100

    Configuración de tailwind con DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import InicioSesion from './components/sesion/InicioSesion'
+import RegistroUsuarios from './components/sesion/RegistroUsuarios'
+import ListaGastos from './components/gastos/ListaGastos'
+import GastosCategoria from './components/gastos/GastosCategoria'
+import EditarGasto from './components/gastos/EditarGasto'
+import Home from './components/Home'
+import { routes } from './routes'
 
 function App() {
-	return <h1 className='text-3xl bg-slate-500 text-center'>Empezar proyecto</h1>
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path={routes.login} element={<InicioSesion />} />
+				<Route path={routes.register} element={<RegistroUsuarios />} />
+				<Route path={routes.listGastos} element={<ListaGastos />} />
+				<Route path={routes.categories} element={<GastosCategoria />} />
+				<Route path={routes.editGasto} element={<EditarGasto />} />
+				<Route index path={routes.home} element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	plugins: [require('daisyui')],
 	theme: {
-		extend: {}
-	},
-	plugins: []
+		container: {
+			// you can configure the container to be centered
+			center: true,
+
+			// or have default horizontal padding
+			padding: '1rem',
+
+			// default breakpoints but with 40px removed
+			screens: {
+				sm: '600px',
+				md: '728px',
+				lg: '984px',
+				xl: '984px',
+				'2xl': '984px'
+			}
+		},
+		plugins: [require('@tailwindcss/typography'), require('daisyui')],
+		daisyui: {
+			styled: true,
+			themes: true,
+			base: true,
+			utils: true,
+			logs: true,
+			rtl: false,
+			prefix: '',
+			darkTheme: 'dark',
+			themes: [
+				'dark',
+				'fantasy',
+				{
+					emisua: {
+						primary: '#ed507a',
+						secondary: '#4edb72',
+						accent: '#ffcccc',
+						neutral: '#2F2235',
+						'base-100': '#FDFCFD',
+						info: '#AEC2EA',
+						success: '#3FE4C3',
+						warning: '#D2A904',
+						error: '#FA3862'
+					}
+				}
+			]
+		}
+	}
 }
